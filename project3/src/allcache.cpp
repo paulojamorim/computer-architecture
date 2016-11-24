c0537a992ea8055185f5fd101bd500b9e2238476
Paulo Henrique Junqueira Amorim <paulojamorim@gmail.com>
2016-11-24 16:49:27 -0200

    Started lz4 implementation

--- project3/src/allcache.cpp
+++ project3/src/allcache.cpp
@@ -37,10 +37,10 @@ END_LEGAL */
  *  instruction+data TLB+cache hieraries
  */
 
-
 #include <iostream>
 #include <fstream>
 
+#include "lz4.h"
 #include "pin.H"
 #include "pinplay.H"
 //#include "cache.H"
@@ -172,10 +172,9 @@ LOCALFUN VOID Fini(int code, VOID * v)
     UINT64 ORIGINAL_SIZE_AS_BYTES;
     ORIGINAL_SIZE_AS_BYTES = COUNT_ORIGINAL_SIZE * 8;
 
-    OutFile << "Original size: " << " " << ORIGINAL_SIZE_AS_BYTES << endl;
-    OutFile << "Intra size: " << " " << COUNT_LBE_INTRA_LINE << endl;
-    OutFile << "Inter size: " << " " << COUNT_LBE_INTER_LINE << endl;
-
+    OutFile << "Original:" << " " << ORIGINAL_SIZE_AS_BYTES << endl;
+    OutFile << "Intra:" << " " << COUNT_LBE_INTRA_LINE << endl;
+    OutFile << "Inter:" << " " << COUNT_LBE_INTER_LINE << endl;
     OutFile.close();
 }
 
@@ -352,6 +351,15 @@ LOCALFUN VOID Instruction(INS ins, VOID *v)
 
 GLOBALFUN int main(int argc, char *argv[])
 {
+
+    const char *src = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus purus et risus vulputate, et mollis orci ullamcorper. Nulla facilisi. Fusce in ligula sed purus varius aliquet interdum vitae justo. Proin quis diam velit. Nulla varius iaculis auctor. Cras volutpat, justo eu dictum pulvinar, elit sem porttitor metus, et imperdiet metus sapien et ante. Nullam nisi nulla, ornare eu tristique eu, dignissim vitae diam. Nulla sagittis porta libero, a accumsan felis sagittis scelerisque.  Integer laoreet eleifend congue. Etiam rhoncus leo vel dolor fermentum, quis luctus nisl iaculis. Praesent a erat sapien. Aliquam semper mi in lorem ultrices ultricies. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In feugiat risus sed enim ultrices, at sodales nulla tristique. Maecenas eget pellentesque justo, sed pellentesque lectus. Fusce sagittis sit amet elit vel varius. Donec sed ligula nec ligula vulputate rutrum sed ut lectus. Etiam congue pharetra leo vitae cursus. Morbi enim ante, porttitor ut varius vel, tincidunt quis justo. Nunc iaculis, risus id ultrices semper, metus est efficitur ligula, vel posuere risus nunc eget purus. Ut lorem turpis, condimentum at sem sed, porta aliquam turpis. In ut sapien a nulla dictum tincidunt quis sit amet lorem. Fusce at est egestas, luctus neque eu, consectetur tortor. Phasellus eleifend ultricies nulla ac lobortis.  Morbi maximus quam cursus vehicula iaculis. Maecenas cursus vel justo ut rutrum. Curabitur magna orci, dignissim eget dapibus vitae, finibus id lacus. Praesent rhoncus mattis augue vitae bibendum. Praesent porta mauris non ultrices fermentum. Quisque vulputate ipsum in sodales pulvinar. Aliquam nec mollis felis. Donec vitae augue pulvinar, congue nisl sed, pretium purus. Fusce lobortis mi ac neque scelerisque semper. Pellentesque vel est vitae magna aliquet aliquet. Nam non dolor. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi ac lacinia felis metus.";
+  // Set and derive sizes.  Since we're using strings, use strlen() + 1 for \0.
+  const size_t src_size = strlen(src) + 1;
+  const size_t max_dst_size = LZ4_compressBound(src_size);
+
+    cout << max_dst_size; 
+
+
     PIN_Init(argc, argv);
 
 
